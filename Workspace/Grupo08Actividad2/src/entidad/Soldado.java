1c3966efd549b0c9f6ee439b7b88e7b552fc59a9
Diego de Silva Setién <dasiga97@gmail.com>
2023-02-15 18:56:27 +0100

    doc Soldado

--- Workspace/Grupo08Actividad2/src/entidad/Soldado.java
+++ Workspace/Grupo08Actividad2/src/entidad/Soldado.java
@@ -1,11 +1,26 @@
 package entidad;
 
+/**
+ * Clase que da forma a la entidad <b>Soldado</b> donde podremos saber si la entidad <b>está viva</b>,
+ * si <b>puede disparar</b> con la munición que tiene; y permite <b>disparar</b> a otro Soldado
+ * @author diego de silva
+ * @version 0.1
+ */
 public class Soldado {
 
+	/**
+	 * Indicador para saber si la entidad está meurta o no.
+	 * @author diego de silva
+	 */
 	private boolean estaMuerto;
+	/**
+	 * Cantidad de balas que dispone el Soldado para su uso 
+	 * @author diego de silva
+	 */
 	private int numeroBalas;
 	
-	
+	//Los get and set no se documentan	
+	//is cambia por get porque es un indicador en los booleans.
 	public boolean isEstaMuerto() {
 		return estaMuerto;
 	}
@@ -19,7 +34,12 @@ public class Soldado {
 		this.numeroBalas = numeroBalas;
 	}
 	
-	
+	/**
+	 * Permite saber si este Soldado tiene balas disponibles (al menos 1)
+	 * 
+	 * @author diego de silva
+	 * @return Devuelve <b>true</b> si tenemos suficientes balas. <p>En caso contrario devuelve <b>false</b>
+	 */
 	public boolean puedeDisparar() {
 		
 		if(this.numeroBalas > 0)
@@ -28,4 +48,16 @@ public class Soldado {
 		return false;
 		
 	}
+	
+	/**
+	 * Utiliza 1 bala para cambiar el estado 
+	 * del <b>Soldado</b> objetivo a <b>muerto</b> 
+	 * @param sol Soldado objetivo que va a morir
+	 */
+    public void disparar(Soldado sol) {
+
+        this.numeroBalas--;
+
+        sol.estaMuerto = true;
+    }  
 }
