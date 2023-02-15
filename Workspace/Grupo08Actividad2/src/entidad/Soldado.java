b92f0decce4849995a8bc2cc32011746872e2e04
Diego de Silva Setién <dasiga97@gmail.com>
2023-02-15 18:04:22 +0100

    setup project

---
+++ Workspace/Grupo08Actividad2/src/entidad/Soldado.java
@@ -0,0 +1,31 @@
+package entidad;
+
+public class Soldado {
+
+	private boolean estaMuerto;
+	private int numeroBalas;
+	
+	
+	public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
+	
+	
+	public boolean puedeDisparar() {
+		
+		if(this.numeroBalas > 0)
+			return true;
+		
+		return false;
+		
+	}
+}
