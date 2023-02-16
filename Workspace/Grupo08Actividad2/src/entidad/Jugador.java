ce45419364981d60290245b8386609a6c2fd2ede
Diego de Silva Setién <dasiga97@gmail.com>
2023-02-16 11:33:12 +0100

    doc jugador

--- Workspace/Grupo08Actividad2/src/entidad/Jugador.java
+++ Workspace/Grupo08Actividad2/src/entidad/Jugador.java
@@ -1,9 +1,27 @@
 package entidad;
 
+/**
+ * Clase que da forma a la entidad "Jugador" que indica el <b>dorsal</b> que lleva, 
+ * y el <b>número de tarjetas amarilla</b> y <b>rojas</b>.
+ * <p>
+ * Puede <b>ponerse un dorsal</b> e indicarnos <b>si está expulsado</b>.
+ * @author diego de silva
+ * @version 0.1
+ */
 public class Jugador {
 
+	/**
+	 * Indicador numérico del jugador
+	 * @author diego de silva
+	 */
 	private int dorsal;
+	/**
+	 * Cantidad de tarjetas amarillas que tiene el jugador
+	 */
 	private int numeroTarjetasAmarillas;
+	/**
+	 * Cantidad de tarjetas rojas que tiene el jugador
+	 */
 	private int numeroTarjetasRojas;
 	
 	
@@ -26,7 +44,11 @@ public class Jugador {
 		this.numeroTarjetasRojas = numeroTarjetasRojas;
 	}
 	
-	
+	/**
+	 * Método que permite cambiar el dorsal al jugador por otro o el mismo siempre que sea un valor comprendido entre 1 y 30 (incluidos)
+	 * @author diego de silva
+	 * @param dorsal número al que se desea cambiar
+	 */
     public void ponerDorsal(int dorsal) {
     	
         if(dorsal >= 1 && dorsal <= 30)                     
@@ -35,7 +57,10 @@ public class Jugador {
         	this.dorsal = -1;
     }
 
-
+    /**
+     * Método para saber si un jugador está expulsado según el <b>número de tarjetas</b> (amarillas y/o rojas) que tenga
+     * @return Devuelve <b>true</b> si tiene 2 amarillas o 1 roja. En caso contrario devuelve <b>false</b>
+     */
     public boolean estaExpulsado() {
 
         boolean expulsado = false;                                                  
